18f8cf56d3e6b9b9f362d225085fe45a9e316d88
RodolphoGLC <rodolpho.geouerj20@gmail.com>
2023-06-12 16:30:49 -0300

    Tela About Us c/ reajustes

--- src/pages/AboutUs/AboutUs.js
+++ src/pages/AboutUs/AboutUs.js
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const ContentContainer = styled.div`
+  width: 100vw;
+  height: auto;
   min-height: 100vh;
   margin-top: 50px;
   display: flex;
@@ -8,6 +10,11 @@ export const ContentContainer = styled.div`
   justify-content: center;
   align-items: center;
   margin-top: 0;
+  background-color: #D6C7E4;
+
+  h3 {
+    margin: 0 0 20px 0;
+  }
 `;
 
 export const UsersContainer = styled.div`
@@ -25,8 +32,9 @@ export const UserCard = styled.div`
     gap: 20px;
     margin: 10px;
     padding: 10px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
+    border: 1px solid #000;
+    border-radius: 15px;
+    background-color: #ad7dd1;
 
     img {
         width: 100px; 
