de2fd40b196576adc7a17356605ae36551aae3d9
AllanFerreiraGomes <allanferreiragomes1@gmail.com>
2023-06-12 17:11:23 -0300

    shop e carrinho

---
+++ src/pages/shop/Shop.js
@@ -0,0 +1,67 @@
+// import styled from "styled-components";
+
+
+// const Container = styled.div`
+//   display: table;
+//   width: 100%;
+// `;
+
+// const CardContainer = styled.div`
+//   display: flex;
+//   flex-wrap: wrap;
+// `;
+
+// const Card = styled.div`
+//   padding: 10px;
+//   border: 1px solid #ccc;
+//   text-align: center;
+//   width: 25%;
+
+
+// `;
+
+
+//   @media (max-width: 768px) {
+//     width: 50%;
+//   }
+// `;
+
+
+
+// background-color: ${props => {
+//     let color = '#FFF';
+
+
+//     switch (props.letterType.toLowerCase()) {
+//       case 'Vento':
+//         color = '#00FF00 ';
+//         break;
+
+//       case 'Trevas':
+//         color = '#800080 ';
+//         break;
+
+//       case 'Terra':
+//         color = '#8B4513 ';
+//         break;
+
+//       case 'Luz':
+//         color = '#FFFF00 ';
+//         break;
+
+//       case 'Fogo':
+//         color = '#FF0000 ';
+//         break;
+
+//       case 'Água':
+//         color = '#0000FF ';
+//         break
+
+//       case 'Divino':
+//         color = '#FFD700 ';
+//         break
+//     }
+//     Descrição
+
+//     return color;
+//   }};
