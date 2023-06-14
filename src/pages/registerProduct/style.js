c70b1e57cb1b513a2203a784cbb7d94557f827d7
caioOliveiraF <caio.o.12oferreirao@gmail.com>
2023-06-13 22:35:53 -0300

    Styled regProd

--- src/pages/registerProduct/style.js
+++ src/pages/registerProduct/style.js
@@ -8,8 +8,31 @@ export const ContentContainer = styled.div`
   place-items: center;
   gap: 20px;
   min-height: 100vh;
+  background-color: #282d32;
+  width: 400px;
+  height: 1000px;
+  border-radius: 13px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  color: white;
 
   padding-bottom: 70px;
+  input[type="text"] {
+    background-color: #601860;
+    border-color: #601860;
+    margin: 5px;
+    border-radius: 5px;
+    text-align: center;
+  }
+
+  input[type="number"] {
+    background-color: #601860;
+    border-color: #601860;
+    margin: 5px;
+    border-radius: 5px;
+    text-align: center;
+    color: white;
+  }
 
   input[type="file"] {
     display: none;
@@ -17,15 +40,32 @@ export const ContentContainer = styled.div`
   .arquivo {
     padding-left: 5px;
     padding-right: 5px;
-    border: solid 1px;
+    color: white ;
+    background-color: #601860;
+    border-radius: 5px;
     cursor: pointer;
   }
+
+  .submit{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    background-color: #601860;
+    border-radius: 13px;
+    border-color: #601860;
+    margin: 5px;
+  }
+  
 `;
 
 export const Card_container = styled.div`
   display: flex;
   flex-direction: column;
-  padding-top: 50px;
+  padding-top: 9px;
+  width: 220px;
+  height: 310px;
+  border-radius: 13px;
 `;
 
 export const Card_body = styled.div`
@@ -54,8 +94,12 @@ export const Card_body = styled.div`
 export const Img_container = styled.div`
   display: flex;
   place-content: center;
+  background-color: #601860;
+  padding: 5px;
+  border-radius: 13px;
 
   img {
+    border-radius: 13px;
     width: 200px;
   }
 `;
