041542414f34e575612838bbb9c88d96065dd5ef
Paulo Vitor Lessa <paulo.vitor.lessa@gmail.com>
2023-06-14 14:55:05 -0300

    NavBar style

--- src/common/components/navBar/style.js
+++ src/common/components/navBar/style.js
@@ -1,16 +1,13 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  a {
-    color: #570e8c;
-  }
   .conta {
     padding-left: 10px;
     padding-right: 10px;
     width: fit-content;
     border-radius: 5px;
-    background: #ad7dd1;
-    color: white;
+    background: white;
+    color: black;
     font-weight: bold;
     margin-right: 30px;
   }
@@ -18,4 +15,5 @@ export const Container = styled.div`
 
 export const P = styled.p`
   margin: 0;
+  color: #ffffff8c;
 `;
