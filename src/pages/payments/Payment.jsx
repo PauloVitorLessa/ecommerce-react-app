d2171436fe9943117b7efe102f0be81a832cb782
Paulo Vitor Lessa <paulo.vitor.lessa@gmail.com>
2023-06-14 17:22:39 -0300

    savePedido na api

--- src/pages/payments/Payment.jsx
+++ src/pages/payments/Payment.jsx
@@ -1,36 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import Modal from "react-bootstrap/Modal";
+import { Api } from "../../services/api";
 
 function Payment() {
   const [paid, setPaid] = useState(false);
-
+  const [produtos, setProdutos] = useState([]);
+  const [cliente, setCliente] = useState([]);
+  const [pedido, setPedido] = useState([]);
+  const [smShow, setSmShow] = useState(false);
+  const [modalTitle, setModalTitle] = useState("");
+  const [modalBody, setModalBody] = useState("");
+  const [pedidoConcluido, setPedidoConcluido] = useState("");
 
   const calculateTotal = () => {
     let total = 0;
-  
+
     const savedCartItems = sessionStorage.getItem("cartItems");
     if (savedCartItems) {
       const cartItems = JSON.parse(savedCartItems);
-  
+
       if (cartItems.length > 0) {
         for (const item of cartItems) {
-          const quantity = item.quantity || 1; //se nao tiver quantidade fica 1 
+          const quantity = item.quantity || 1; //se nao tiver quantidade fica 1
           total += item.valorUnitario * quantity;
         }
       }
     }
-  
+
     return total;
   };
-  
 
   const product = {
     price: calculateTotal(),
-  
   };
 
   const paypalOptions = {
-    "client-id": "AUpdxl3_a0LpqFLg8qm8ZF5XyCIhVtxGMQ1nNYp5tJjxLJexV7gkZUoqfbon40nb97LGQ7WhbXJtIHiz",
+    "client-id":
+      "AUpdxl3_a0LpqFLg8qm8ZF5XyCIhVtxGMQ1nNYp5tJjxLJexV7gkZUoqfbon40nb97LGQ7WhbXJtIHiz",
     currency: "BRL",
   };
 
@@ -38,7 +45,6 @@ function Payment() {
     return actions.order.create({
       purchase_units: [
         {
-         
           amount: {
             currency_code: "BRL",
             value: product.price,
@@ -55,108 +61,126 @@ function Payment() {
   //   });
   // };
 
-//------------------------------------------------------------------
+  //------------------------------------------------------------------
+  useEffect(() => {
+    function getProductsFromSession() {
+      const products = JSON.parse(sessionStorage.getItem("cartItems"));
+      setProdutos(products);
 
+      const cliente = JSON.parse(sessionStorage.getItem("user"));
+      setCliente(cliente);
+    }
+    getProductsFromSession();
+  }, []);
 
-const getProductsFromSession = (session) => {
-  const products = [];
+  //============================================================
 
-  for (let i = 0; i < session.length; i++) {
-    const product = session[i];
-    products.push({
-      id: product.idProduto,
-      nome: product.nome,
-      descricao: product.descricao,
-      categoria: product.nomeCategoria,
-      quantidade: product.quantity,
-      valorUnitario: product.valorUnitario
-    });
-  }
-  return products;
-  console.log(products)
-};
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then((details) => {
+      setPaid(true);
+      console.log(details);
+      console.log("detalhes");
 
-
-
-//============================================================
-
-const onApprove = (data, actions) => {
-return actions.order.capture().then((details) => {
-  setPaid(true);
-  console.log(details);
-
-  const savedCartItems = sessionStorage.getItem("cartItems");
-  if (savedCartItems) {
-    const cartItems = JSON.parse(savedCartItems);
-
-    const pedidos = cartItems.map((item) => {
-      const pedido = {
-        // Dados do pedido a serem enviados para a API 
+      const pedidoJson = {
+        status: "aberto",
+        cliente: {
+          idCliente: cliente.id,
+        },
       };
 
-      return axios
-        .post("https://api-restful-trabalho-final-production.up.railway.app/api/pedidos", pedido)
-        .then((response) => response.data)
-        .catch((error) => {
-          console.error("Erro ao enviar solicitação para a API 'pedidos':", error);
+      Api.post("/pedidos", pedidoJson)
+        .then((res) => {
+          setPedido(res.data);
+          produtos.forEach((prod, index) => {
+            let itemPedidoJson;
+
+            if (index + 1 == produtos.length) {
+              itemPedidoJson = {
+                quantidade: prod.quantity,
+                percentualDesconto: 0,
+                produto: {
+                  idProduto: prod.idProduto,
+                },
+                pedido: {
+                  idPedido: res.data.idPedido,
+                },
+                status: "fechado",
+              };
+            } else {
+              itemPedidoJson = {
+                quantidade: prod.quantity,
+                percentualDesconto: 0,
+                produto: {
+                  idProduto: prod.idProduto,
+                },
+                pedido: {
+                  idPedido: res.data.idPedido,
+                },
+                status: "aberto",
+              };
+            }
+
+            Api.post("/itempedidos", itemPedidoJson)
+              .then((res) => {
+                if (res.data.status == "fechado") {
+                  setPedidoConcluido(true);
+                  setModalTitle("Sucesso");
+                  setModalBody("Pedido Realizado");
+                  setSmShow(true);
+                }
+              })
+              .catch((err) => {
+                console.log("erro na api itemPedidos");
+                console.log(err);
+                setModalTitle("Erro");
+                setModalBody("Errou ao adicionar um produto no Pedido");
+                setSmShow(true);
+              });
+          });
+        })
+        .catch((err) => {
+          console.log("erro na api pedidos");
+          console.log(err);
+          setModalTitle("Erro");
+          setModalBody("Errou ao Criar o Pedido");
+          setSmShow(true);
         });
     });
+  };
 
-    Promise.all(pedidos)
-      .then((orderedItems) => {
-        // Extrair os IDs dos pedidos criados
-        const pedidoIDs = orderedItems.map((item) => item.id);
-
-        const itensPedidos = cartItems.map((item, index) => {
-          const itensPedido = {
-            // Dados dos itens de pedido a serem enviados para a API "itensPedidos"
-            pedidoId: pedidoIDs[index],
-            // Outros dados dos itens de pedido
-          };
-
-          return axios
-            .post("https://api-restful-trabalho-final-production.up.railway.app/api/itensPedidos", itensPedido)
-            .then((response) => response.data)
-            .catch((error) => {
-              console.error("Erro ao enviar solicitação para a API 'itensPedidos':", error);
-            });
-        });
-
-        return Promise.all(itensPedidos);
-      })
-      .then((orderedItensPedidos) => {
-        console.log("Pedidos:", orderedItems);
-        console.log("Itens de Pedidos:", orderedItensPedidos);
-      })
-      .catch((error) => {
-        console.error("Erro ao enviar solicitações para a API:", error);
-      });
-  }
-});
-};
-
-
-
-
-//--------------------------------------------------------------
+  //--------------------------------------------------------------
   return (
-    <div className="Payment">
-      {paid ? (
-        <div>
-          <h1>compra realizada</h1>
-       
-        </div>
-      ) : (
-        <>
-          <h1>
-            {product.description} por R${product.price}
-          </h1>
-          <PayPalScriptProvider options={paypalOptions}>
-            <PayPalButtons createOrder={createOrder} onApprove={onApprove} />
-          </PayPalScriptProvider>
-        </>
-      )}
-    </div>
+    <>
+      <div className="Payment">
+        {paid ? (
+          <div>
+            <h1>compra realizada</h1>
+          </div>
+        ) : (
+          <>
+            <h1>
+              {product.description} por R${product.price}
+            </h1>
+            <PayPalScriptProvider options={paypalOptions}>
+              <PayPalButtons createOrder={createOrder} onApprove={onApprove} />
+            </PayPalScriptProvider>
+          </>
+        )}
+      </div>
+      <Modal
+        size="sm"
+        show={smShow}
+        onHide={() => setSmShow(false)}
+        aria-labelledby="example-modal-sizes-title-sm"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-sm">
+            {modalTitle}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{modalBody}</Modal.Body>
+      </Modal>
+    </>
   );
 }
 
