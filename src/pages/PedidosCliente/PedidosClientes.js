72352c9a770674d4be1196ed541dc074fd1f1728
Paulo Vitor Lessa <paulo.vitor.lessa@gmail.com>
2023-06-14 18:22:32 -0300

    bug fix

--- src/pages/PedidosCliente/PedidosClientes.js
+++ src/pages/PedidosCliente/PedidosClientes.js
@@ -1,42 +1,59 @@
 import styled from "styled-components";
 
 export const ContentContainer = styled.div`
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
-    width: 100vw;
-    height: auto;
-    min-height: 100vh;
-    flex-direction: column;
-    background-color: #ad7dd1;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 100vw;
+  height: auto;
+  min-height: 100vh;
+  flex-direction: column;
+  background-color: #ad7dd1;
 `;
 
 export const Title = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 2rem;
-    background-color: #282d32;
-    color: white;
-    margin-top: 50px;
-    padding: 10px;
-    border-radius: 13px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 2rem;
+  background-color: #282d32;
+  color: white;
+  margin-top: 50px;
+  padding: 10px;
+  border-radius: 13px;
 `;
 
 export const Pedido = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 500px;
-    background-color: #282d32;
-    text-align: center;
-    padding: 10px 0;
-    border-radius: 13px;
+  margin-top: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  background-color: #282d32;
+  text-align: center;
+  padding: 10px 0;
+  border-radius: 13px;
+  color: white;
+
+  h3 {
     color: white;
+    margin: 0 0 20px 0;
+  }
+`;
 
-    h3 {
-        color: white;
-        margin: 0 0 20px 0;
+export const ItemPedido = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  background-color: #282d32;
+  text-align: center;
+  padding: 10px 0;
+  border-radius: 13px;
+  color: white;
 
-    }
+  h3 {
+    color: white;
+    margin: 0 0 20px 0;
+  }
 `;
--- src/pages/PedidosCliente/PedidosClientes.jsx
+++ src/pages/PedidosCliente/PedidosClientes.jsx
@@ -6,63 +6,83 @@
 
 import { useEffect, useState } from "react";
 import { Api } from "../../services/api";
-import { ContentContainer, Title, Pedido } from './PedidosClientes';
+import { ContentContainer, Title, Pedido, ItemPedido } from "./PedidosClientes";
 
 const PedidosClientes = () => {
+  const [pedidosCliente, setPedidosClientes] = useState([]);
+  const [itemPedidos, setItemPedidos] = useState([]);
+  const [pedidos, setPedidos] = useState([]);
 
-    const [pedidosCliente, setPedidosClientes] = useState([]);
+  useEffect(() => {
+    fetchPedidos();
+  }, []);
 
-    useEffect(() => {
-        fetchPedidos();
-    }, []);
+  const fetchPedidos = async () => {
+    const idCliente = JSON.parse(sessionStorage.getItem("user")).id;
 
-    const fetchPedidos = async () => {
-        const idCliente = JSON.parse(sessionStorage.getItem("user")).id;
+    const pedidosTemp = await Api.get(`/pedidos`);
 
-        const pedidosTemp = await Api.get(`/pedidos`);
-        
-        const pedidosClienteId = pedidosTemp.data.filter(pedido => pedido.cliente.idCliente === idCliente);
-        setPedidosClientes(pedidosClienteId);
-    }
-
-    const verificarData = (dataPedido, dataEntrega, dataEnvio) => {
-        if(dataPedido !== null && dataEntrega === null && dataEnvio === null) {
-            return "----";
-        }
-        else if(dataPedido !== null && dataEnvio === null && dataEntrega !== null ) {
-            return "Data Envio: " + dataEnvio;
-        }
-        else if(dataPedido !== null && dataEnvio !== null && dataEntrega !== null) {
-            return "Data Entrega: " + dataEntrega;
-        }
-        else {
-            return "Data não fornecida"
-        }
-    }
-
-    return (
-        <>
-        <ContentContainer className="containerPedidos">
-            <Title>Pedidos</Title> 
-            {pedidosCliente ? ( 
-                pedidosCliente.map(pedido => {
-                    return (
-                        <Pedido key={pedido.idPedido}>
-                            <div className="infoPedido">
-                                <h3>Pedido: {pedido.idPedido} - Status: {pedido.status}</h3>
-                                <p>Data Pedido: {pedido.dataPedido}</p>
-                                <p>{verificarData(pedido.dataPedido, pedido.dataEnvio, pedido.dataEntrega)}</p>
-                                <h4>Valor Total: R${pedido.valorTotal}</h4>
-                            </div> 
-                        </Pedido>
-                    )
-                })
-            ) : (
-                <p>Não foi encontrado nenhum pedido em seu nome</p>
-            )}
-        </ContentContainer>
-        </>
+    const pedidosClienteId = pedidosTemp.data.filter(
+      (pedido) => pedido.cliente.idCliente === idCliente
     );
+    setPedidosClientes(pedidosClienteId);
+    setItemPedidos(pedidosClienteId[0].cliente.pedido[0].itensPedidos);
+  };
+
+  const verificarData = (dataPedido, dataEntrega, dataEnvio) => {
+    if (dataPedido !== null && dataEntrega === null && dataEnvio === null) {
+      return "----";
+    } else if (
+      dataPedido !== null &&
+      dataEnvio === null &&
+      dataEntrega !== null
+    ) {
+      return "Data Envio: " + dataEnvio;
+    } else if (
+      dataPedido !== null &&
+      dataEnvio !== null &&
+      dataEntrega !== null
+    ) {
+      return "Data Entrega: " + dataEntrega;
+    } else {
+      return "Data não fornecida";
+    }
+  };
+
+  return (
+    <>
+      <ContentContainer className="containerPedidos">
+        <Title>Pedidos</Title>
+        {pedidosCliente ? (
+          pedidosCliente.map((pedido) => {
+            return (
+              <Pedido key={pedido.idPedido}>
+                <div className="infoPedido">
+                  <h3>
+                    Pedido: {pedido.idPedido} - Status: {pedido.status}
+                  </h3>
+
+                  <p>
+                    Data Pedido:{" "}
+                    {pedido.dataPedido
+                      .slice(0, 10)
+                      .split("-")
+                      .reverse()
+                      .join("/")}
+                  </p>
+
+                  <h4>Valor Total: R${pedido.valorTotal}</h4>
+                  <ItemPedido></ItemPedido>
+                </div>
+              </Pedido>
+            );
+          })
+        ) : (
+          <p>Não foi encontrado nenhum pedido em seu nome</p>
+        )}
+      </ContentContainer>
+    </>
+  );
 };
 
 export default PedidosClientes;
